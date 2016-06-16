b4dc3ab761812bf05f0035abe0486690a51daf33
Cosinity <accostenoble@gmail.com>
2016-06-16 09:54:22 -0400

    Added IMusicView interface

---
+++ IMusicView.java
@@ -0,0 +1,14 @@
+package cs3500.music.view;
+
+import cs3500.music.model.GenericMusicPiece;
+
+/**
+ * Represents the functionality of a MusicPlayer view
+ */
+public interface IMusicView {
+  /**
+   * Plays the given piece in the manner of this view (e.g. displays it to a GUI, plays the
+   * notes' audio, etc.)
+   */
+  void play(GenericMusicPiece piece);
+}
