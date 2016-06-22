3b4a193d2362737c2369db222747d75a9d86543b
Cosinity <accostenoble@gmail.com>
2016-06-22 10:31:54 -0400

    Added new view interface

---
+++ InteractableView.java
@@ -0,0 +1,46 @@
+package cs3500.music.view;
+
+import cs3500.music.model.Note;
+
+/**
+ * Represents the functionality of a view for MusicPlayer with interactability
+ */
+public interface InteractableView<T> extends IMusicView<T> {
+  // Note-specific methods
+  /**
+   * Selects the given note. Specifically, puts a colored outline around the note and updates the
+   * text fields with its parameters.
+   */
+  void selectNote(Note n);
+
+  /**
+   * Gets the input from the duration text field
+   */
+  String getNewDuration();
+
+  /**
+   * Gets the input from the starting beat text field
+   */
+  String getNewStartBeat();
+
+  /**
+   * Gets the input from the pitch text field
+   */
+  String getNewPitch();
+
+  // Piece-wide methods
+  /**
+   * Gets the input from the tempo text field
+   */
+  String getNewTempo();
+
+  /**
+   * Gets the input from the transposition text field
+   */
+  String getTranspose();
+
+  /**
+   * Returns whichever note is at the given x and y coordinates
+   */
+  T noteAtPos(int x, int y);
+}
